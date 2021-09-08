db254225925b37c8af4a5e8ad1ec7861c7067862
DonNavinS <seneviratnanavin@gmail.com>
2021-09-08 17:06:49 -0400

    first commit

---
+++ Desktop/React-challenge/react-portfolio/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+import Nav from "./components/Nav";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <header>
+          <Nav />
+        </header>
+      </div>
+      <div></div>
+      <footer></footer>
+    </div>
+  );
+}
+
+export default App;
