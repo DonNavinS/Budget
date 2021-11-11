5eea7086a7964c1e6077c33472d56dacb392471a
DonNavinS <seneviratnanavin@gmail.com>
2021-11-10 20:46:10 -0500

    front end input can now be added to the database

--- Desktop/Local Restaurants/client/food-finder/src/App.js
+++ Desktop/Local Restaurants/client/food-finder/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Axios from "axios";
+import { useState } from "react";
 
 function App() {
+  const [restName, setRestName] = useState("");
+  const [restDesc, setRestDesc] = useState("");
+  const updateTable = () => {
+    Axios.post("http://localhost:3001/test", {
+      name: restName,
+      description: restDesc,
+    }).then(() => {
+      alert("DB TABLE UPDATED");
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input
+          type="text"
+          name="restName"
+          onChange={(e) => {
+            setRestName(e.target.value);
+          }}
+        ></input>
+        <input
+          type="text"
+          name="restDesc"
+          onChange={(e) => {
+            setRestDesc(e.target.value);
+          }}
+        ></input>
+        <button onClick={updateTable}>Submit</button>
+      </div>
     </div>
   );
 }
--- Desktop/Local Restaurants/server/index.js
+++ Desktop/Local Restaurants/server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mysql = require("mysql2");
 require("dotenv").config();
+const cors = require("cors");
 
 const db = mysql.createConnection({
   host: "localhost",
@@ -10,11 +11,23 @@ const db = mysql.createConnection({
   database: "food_finder",
 });
 
+app.use(cors());
+app.use(express.json());
+
 app.get("/", (req, res) => {
   const sql =
-    'INSERT INTO all_restaurants (name, description) VALUES ("REST6", "DESC6");';
+    'INSERT INTO all_restaurants (name, description) VALUES ("Test1", "Test2");';
   db.query(sql, (err, result) => {
     res.send("TABLE UPDATED");
+  });
+});
+app.post("/test", (req, res) => {
+  const name = req.body.name;
+  const description = req.body.description;
+  const sql = "INSERT INTO all_restaurants (name, description) VALUES (?,?);";
+  db.query(sql, [name, description], (err, result) => {
+    res.send("TABLE UPDATED 2");
+    console.log(err);
   });
 });
 
